799daf1a7f4c8eafe57704aa2ba2f6b9ea9e6778
rleathart <56587304+rleathart@users.noreply.github.com>
2020-10-07 15:30:17 +0100

    Implement game with move checking

--- main.c
+++ main.c
@@ -1,44 +1,26 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
+/* An even number that defines the number of row/columns in the grid. */
 #define GRID_SIZE 8
 
-char *grid[GRID_SIZE][GRID_SIZE];
+static int grid[GRID_SIZE][GRID_SIZE] = {0};
+
 bool Player1Turn = true;
-bool PlayerColour;
+int PlayerColour;
 
-void PrintGrid(char *grid[GRID_SIZE][GRID_SIZE])
+bool ScanLine(int i, int j, bool doFlip)
 {
-  for (int i = 0; i < GRID_SIZE; ++i)
-  {
-    for (int j = 0; j < GRID_SIZE; ++j)
-    {
-      char *DisplayString;
-      if (grid[i][j])
-        DisplayString = grid[i][j];
-      else
-        DisplayString = " ";
-
-      if (j == GRID_SIZE - 1)
-        printf("[%s]\n", DisplayString);
-      else
-        printf("[%s] ", DisplayString);
-    }
-  }
-}
-
-bool CheckLine(int i, int j)
-{
-  if (!grid[i][j]) // If there's an empty square in this line
-    return false;
-  return true;
-}
-
-bool ScanLine(int Position[], char *Direction)
-{
-  int i = Position[0];
-  int j = Position[1];
+  /* Check outwards from the grid position.
+   * Store the position of all tiles after the starting position in some array
+   * toFlip[].
+   * If we encounter a blank tile before a tile of the same colour, illegal
+   * move.
+   * Otherwise, keep adding to toFlip until we reach a tile of the same
+   * colour.
+   * Then loop over toFlip[] inverting the tile colour.
+   */
   char *UpLeft = "upleft";
   char *UpRight = "upright";
   char *DownLeft = "downleft";
@@ -48,122 +30,232 @@ bool ScanLine(int Position[], char *Direction)
   char *Left = "left";
   char *Right = "right";
 
-  if (Direction == Left)
+  char *Directions[] = {UpLeft, UpRight, DownLeft, DownRight,
+                        Up,     Down,    Left,     Right};
+
+  int Starti = i;
+  int Startj = j;
+
+  int StartColour = grid[i][j] ? grid[i][j] : PlayerColour;
+
+  bool didFlip = false;
+  for (int n = 0; n < 8; ++n)
   {
-    while (j >= 0)
+    i = Starti;
+    j = Startj;
+    char *Direction = Directions[n];
+
+    /* Need to initialise toFlip values to {-1,-1} otherwise grid[0][0] will
+     * always be flipped. */
+    int toFlip[GRID_SIZE][2];
+    for (int i = 0; i < GRID_SIZE; ++i)
     {
-      if (!CheckLine(i, j--))
-        return false;
+      toFlip[i][0] = -1;
+      toFlip[i][1] = -1;
     }
-  }
-  if (Direction == Right)
-  {
-    while (j < GRID_SIZE)
+    int FlipCounter = 0;
+    bool validMove = true;
+    while (i >= 0 && i < GRID_SIZE && j >= 0 && j < GRID_SIZE)
     {
-      CheckLine(i, j++);
+      if (Direction == Up)
+        --i;
+      if (Direction == Down)
+        ++i;
+      if (Direction == Left)
+        --j;
+      if (Direction == Right)
+        ++j;
+      if (Direction == UpLeft)
+      {
+        --i;
+        --j;
+      }
+      if (Direction == UpRight)
+      {
+        --i;
+        ++j;
+      }
+      if (Direction == DownLeft)
+      {
+        ++i;
+        --j;
+      }
+      if (Direction == DownRight)
+      {
+        ++i;
+        ++j;
+      }
+
+      /* If we're out of bounds after iterating, move on to the next
+       * direction. */
+      if (!(i >= 0 && i < GRID_SIZE && j >= 0 && j < GRID_SIZE))
+      {
+        validMove = false;
+        break;
+      }
+
+      /* If we encounter a piece that is the same colour as that of the tile
+       * we started on, we have checked all we need to for this direction. */
+      if (grid[i][j] == StartColour)
+        break;
+
+      /* If we encounter a blank tile before a tile of the same colour, this
+       * cannot be a valid direction so move on to the next one. */
+      if (grid[i][j] == 0)
+      {
+        validMove = false;
+        break;
+      }
+
+      /* Store the tiles that we have traversed in toFlip so we can flip them
+       * later if we have to. */
+      toFlip[FlipCounter][0] = i;
+      toFlip[FlipCounter][1] = j;
+
+      ++FlipCounter;
     }
+
+    if (!validMove)
+      continue;
+
+    for (int i = 0; i < 8; ++i)
+    {
+      /* If there isn't at least one tile to flip then check the next
+       * direction. */
+      if (toFlip[i][0] != -1 && toFlip[i][1] != -1)
+        break;
+      goto NEXT_DIRECTION;
+    }
+
+    /* We want to actually flip the pieces and not just check if they need to
+     * be flipped. */
+    if (doFlip)
+    {
+      for (int loop = 0; loop < GRID_SIZE; ++loop)
+      {
+        int row = toFlip[loop][0];
+        int col = toFlip[loop][1];
+
+        if (row >= 0 && col >= 0 && row < GRID_SIZE && col < GRID_SIZE)
+          grid[row][col] = -grid[row][col];
+      }
+    }
+    didFlip = true;
+  NEXT_DIRECTION:
+    continue;
   }
-  return true;
+  return didFlip;
+}
+
+void PrintGrid(int grid[GRID_SIZE][GRID_SIZE])
+{
+  for (int i = 0; i < GRID_SIZE; ++i)
+    for (int j = 0; j < GRID_SIZE; ++j)
+    {
+      char *DisplayString, *TileSep;
+      DisplayString = grid[i][j] == -1 ? "W" : grid[i][j] == 1 ? "B" : " ";
+      /* Print a space between tiles and a newline at the end of a row. */
+      TileSep = j == GRID_SIZE - 1 ? "\n" : " ";
+
+      printf("[%s]%s", DisplayString, TileSep);
+    }
+}
+
+bool isValidMove(int Move[])
+{
+  /* This tile is occupied already. */
+  if (grid[Move[0]][Move[1]] != 0)
+    return false;
+  /* Check if this move will result in any pieces being flipped. */
+  return ScanLine(Move[0], Move[1], false);
 }
 
 int *GetPlayerMove(void)
 {
   static int Move[2];
-  printf("Move vertical 0-7: ");
+  printf("[%s] Move vertical 0-%d: ", Player1Turn ? "W" : "B", GRID_SIZE - 1);
   scanf("%d", &Move[0]);
-  printf("Move horizontal 0-7: ");
+  printf("[%s] Move horizontal 0-%d: ", Player1Turn ? "W" : "B", GRID_SIZE - 1);
   scanf("%d", &Move[1]);
 
-  if (!grid[Move[0]][Move[1]])
-    // TODO: Check if move is actually valid.
-    return Move;
-  else
+  if (grid[Move[0]][Move[1]])
   {
     printf("Grid location %d,%d is already occupied!\n", Move[0], Move[1]);
-    GetPlayerMove();
+    return GetPlayerMove();
+  }
+  else if (!isValidMove(Move))
+  {
+    printf("Invalid move!\n");
+    return GetPlayerMove();
   }
   return Move;
 }
 
-bool CheckForSurround(int x, int y)
-{
-  /* TODO: Need to fix this to check in all straight line directions and
-   * swap all pieces of there are two anchoring pieces of the opposite
-   * colour. */
-  bool isSurrounded = false;
-
-  char *OpponentColour = "";
-  char *GridColour = grid[x][y];
-  char *White = "W";
-  char *Black = "B";
-
-  if (GridColour == White)
-    OpponentColour = "B";
-  else if (GridColour == Black)
-    OpponentColour = "W";
-
-  // Left-right
-  if (grid[x - 1][y] == OpponentColour && grid[x + 1][y] == OpponentColour)
-    isSurrounded = true;
-  // Diagonal
-  if (grid[x - 1][y - 1] == OpponentColour && grid[x + 1][y + 1] == OpponentColour)
-    isSurrounded = true;
-  if (grid[x + 1][y - 1] == OpponentColour && grid[x - 1][y + 1] == OpponentColour)
-    isSurrounded = true;
-  // Vertical
-  if (grid[x][y - 1] == OpponentColour && grid[x][y + 1] == OpponentColour)
-    isSurrounded = true;
-
-  if (isSurrounded)
-  {
-    grid[x][y] = OpponentColour;
-  }
-
-  return isSurrounded;
-}
-
 void DoPlayerMove(int Move[])
 {
-  if (Player1Turn)
-  {
-    grid[Move[0]][Move[1]] = "W";
-  }
-  else
-  {
-    grid[Move[0]][Move[1]] = "B";
-  }
+  grid[Move[0]][Move[1]] = Player1Turn ? -1 : 1;
+  ScanLine(Move[0], Move[1], true);
+  Player1Turn = !Player1Turn;
+}
 
+bool CanPlay(void)
+{
+  /* For every tile on the grid, see if putting a piece there results in a
+   * valid move. If no tiles result in valid moves, the game is over. */
   for (int i = 0; i < GRID_SIZE; ++i)
-  {
     for (int j = 0; j < GRID_SIZE; ++j)
     {
-      CheckForSurround(i, j);
+      int Move[] = {i, j};
+      if (isValidMove(Move))
+        return true;
     }
-  }
+  return false;
+}
 
-  Player1Turn = !Player1Turn;
+int GetScore(int Player)
+{
+  /* Count number of pieces on the grid for a given player.
+   * Player 1: -1
+   * Player 2:  1 */
+  int Score = 0;
+  for (int i = 0; i < GRID_SIZE; ++i)
+    for (int j = 0; j < GRID_SIZE; ++j)
+      if (grid[i][j] == Player)
+        ++Score;
+
+  return Score;
+}
+
+int randrange(int lower, int upper)
+{
+  /* Return a random int between lower and upper inclusive. */
+  return (rand() % (upper - lower + 1)) + lower;
 }
 
 int main(int argc, char *argv[])
 {
-  grid[3][3] = "W";
-  grid[4][4] = "W";
-  grid[3][4] = "B";
-  grid[4][3] = "B";
-
+  /* Initialise starting grid positions. */
+  grid[GRID_SIZE / 2 - 1][GRID_SIZE / 2 - 1] = 1;
+  grid[GRID_SIZE / 2][GRID_SIZE / 2] = 1;
+  grid[GRID_SIZE / 2 - 1][GRID_SIZE / 2] = -1;
+  grid[GRID_SIZE / 2][GRID_SIZE / 2 - 1] = -1;
   while (true)
   {
+    PlayerColour = Player1Turn ? -1 : 1;
+    if (!CanPlay())
+    {
+      int Player1Score = GetScore(-1);
+      int Player2Score = GetScore(1);
+
+      if (Player1Score != Player2Score)
+        printf("Player %d wins!\n", Player1Score > Player2Score ? 1 : 2);
+      else
+        printf("Draw!\n");
+      return 0;
+    }
     PrintGrid(grid);
     int *Move = GetPlayerMove();
     DoPlayerMove(Move);
-    printf("%s\n", ScanLine(Move, "left") ? "true" : "false");
   }
-
-  /* For any grid pos [x][y] the surrounding squares are
-   * [x-1][y-1] [x][y-1] [x+1][y-1]
-   * [x-1][y]   [x][y]   [x+1][y]
-   * [x-1][y+1] [x][y+1] [x+1][y+1]
-   */
-
   return 0;
 }
