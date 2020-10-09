25159686e7270f982b7a2f6c7ef1deefcd006f95
rleathart <56587304+rleathart@users.noreply.github.com>
2020-10-09 01:33:45 +0100

    Refactor, GetFlips in favour of ScanLine.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* An even number that defines the number of row/columns in the grid. */
 #define GRID_SIZE 8
@@ -10,18 +11,39 @@ int grid[GRID_SIZE][GRID_SIZE];
 bool Player1Turn = true;
 int PlayerColour;
 
-bool ScanLine(int i, int j, bool doFlip)
-{
-  /* Check outwards from the grid position.
-   * Store the position of all tiles after the starting position in some array
-   * toFlip[].
-   * If we encounter a blank tile before a tile of the same colour, illegal
-   * move.
-   * Otherwise, keep adding to toFlip until we reach a tile of the same
-   * colour.
-   * Then loop over toFlip[] inverting the tile colour.
-   */
+typedef struct GridInfo GridInfo;
 
+struct GridInfo
+{
+  int Position[GRID_SIZE * GRID_SIZE][2];
+  void (*print)(GridInfo *FlipInfo);
+};
+
+void GridInfo_print(GridInfo *self)
+{
+  int GridInfoPositionSize = sizeof self->Position / sizeof self->Position[0];
+  for (int i = 0; i < GridInfoPositionSize; ++i)
+  {
+    printf("{%d, %d},",self->Position[i][0], self->Position[i][1]);
+  }
+  printf("\n");
+}
+
+GridInfo New_GridInfo(void)
+{
+  GridInfo self;
+  int GridInfoPositionSize = sizeof self.Position / sizeof self.Position[0];
+  for (int i = 0; i < GridInfoPositionSize; ++i)
+  {
+    self.Position[i][0] = -1;
+    self.Position[i][1] = -1;
+  }
+  self.print = GridInfo_print;
+  return self;
+}
+
+GridInfo GetFlips(int i, int j)
+{
   int Directions[8][2] = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
                           {-1, 0},  {1, 0},  {0, -1}, {0, 1}};
 
@@ -30,25 +52,18 @@ bool ScanLine(int i, int j, bool doFlip)
 
   int StartColour = grid[i][j] ? grid[i][j] : PlayerColour;
 
-  bool didFlip = false;
-  for (int n = 0; n < 8; ++n)
+  GridInfo FlipInfo = New_GridInfo();
+
+  int FlipCounter = 0;
+  for (int n = 0; n < sizeof Directions / sizeof Directions[0]; ++n)
   {
     i = Starti;
     j = Startj;
     int *Direction = Directions[n];
 
-    /* Need to initialise toFlip values to {-1,-1} otherwise grid[0][0] will
-     * always be flipped. */
-    int toFlip[GRID_SIZE][2];
-    for (int i = 0; i < GRID_SIZE; ++i)
-    {
-      toFlip[i][0] = -1;
-      toFlip[i][1] = -1;
-    }
-
-    int FlipCounter = 0;
+    int ProgressCheck[GRID_SIZE * GRID_SIZE] = {0};
     bool validMove = true;
-    while (i >= 0 && i < GRID_SIZE && j >= 0 && j < GRID_SIZE)
+    while (true)
     {
       i += Direction[0];
       j += Direction[1];
@@ -76,42 +91,39 @@ bool ScanLine(int i, int j, bool doFlip)
 
       /* Store the tiles that we have traversed in toFlip so we can flip them
        * later if we have to. */
-      toFlip[FlipCounter][0] = i;
-      toFlip[FlipCounter][1] = j;
+      FlipInfo.Position[FlipCounter][0] = i;
+      FlipInfo.Position[FlipCounter][1] = j;
+
+      ProgressCheck[FlipCounter] = 1;
 
       ++FlipCounter;
     }
 
+    /* If this direction isn't valid, reset all the positions that we staged
+     * to be flipped. */
     if (!validMove)
-      continue;
-
-    for (int i = 0; i < 8; ++i)
-    {
-      /* If there isn't at least one tile to flip then check the next
-       * direction. */
-      if (toFlip[i][0] != -1 && toFlip[i][1] != -1)
-        break;
-      goto NEXT_DIRECTION;
-    }
-
-    /* We want to actually flip the pieces and not just check if they need to
-     * be flipped. */
-    if (doFlip)
-    {
-      for (int loop = 0; loop < GRID_SIZE; ++loop)
-      {
-        int row = toFlip[loop][0];
-        int col = toFlip[loop][1];
-
-        if (row >= 0 && col >= 0 && row < GRID_SIZE && col < GRID_SIZE)
-          grid[row][col] = -grid[row][col];
-      }
-    }
-    didFlip = true;
-  NEXT_DIRECTION:
-    continue;
+      for (int i = 0; i < sizeof ProgressCheck / sizeof ProgressCheck[0]; ++i)
+        if (ProgressCheck[i] == 1)
+        {
+          FlipInfo.Position[i][0] = -1;
+          FlipInfo.Position[i][1] = -1;
+        }
   }
-  return didFlip;
+  return FlipInfo;
+}
+
+void DoFlips(GridInfo FlipInfo)
+{
+  int FlipInfoPositionSize =
+      sizeof FlipInfo.Position / sizeof FlipInfo.Position[0];
+
+  for (int i = 0; i < FlipInfoPositionSize; ++i)
+  {
+    int row = FlipInfo.Position[i][0];
+    int col = FlipInfo.Position[i][1];
+    if (row != -1 && col != -1)
+      grid[row][col] = -grid[row][col];
+  }
 }
 
 void PrintGrid(int grid[GRID_SIZE][GRID_SIZE])
@@ -128,13 +140,25 @@ void PrintGrid(int grid[GRID_SIZE][GRID_SIZE])
     }
 }
 
-bool isValidMove(int Move[])
+bool isValidMove(int Move[], GridInfo FlipInfo)
 {
   /* This tile is occupied already. */
   if (grid[Move[0]][Move[1]] != 0)
     return false;
-  /* Check if this move will result in any pieces being flipped. */
-  return ScanLine(Move[0], Move[1], false);
+
+  int FlipInfoPositionSize =
+      sizeof FlipInfo.Position / sizeof FlipInfo.Position[0];
+
+  for (int i = 0; i < FlipInfoPositionSize; ++i)
+  {
+    int row = FlipInfo.Position[i][0];
+    int col = FlipInfo.Position[i][1];
+
+    /* If this move results in at least one flip. */
+    if (row != -1 && col != -1)
+      return true;
+  }
+  return false;
 }
 
 int *GetPlayerMove(void)
@@ -150,22 +174,17 @@ int *GetPlayerMove(void)
     printf("Grid location %d,%d is already occupied!\n", Move[0], Move[1]);
     return GetPlayerMove();
   }
-  else if (!isValidMove(Move))
-  {
-    printf("Invalid move!\n");
-    return GetPlayerMove();
-  }
   return Move;
 }
 
-void DoPlayerMove(int Move[])
+void DoPlayerMove(int Move[], GridInfo FlipInfo)
 {
   grid[Move[0]][Move[1]] = Player1Turn ? -1 : 1;
-  ScanLine(Move[0], Move[1], true);
+  DoFlips(FlipInfo);
   Player1Turn = !Player1Turn;
 }
 
-bool CanPlay(void)
+bool CanPlay(GridInfo FlipInfo)
 {
   /* For every tile on the grid, see if putting a piece there results in a
    * valid move. If no tiles result in valid moves, the game is over. */
@@ -173,7 +192,10 @@ bool CanPlay(void)
     for (int j = 0; j < GRID_SIZE; ++j)
     {
       int Move[] = {i, j};
-      if (isValidMove(Move))
+      if (grid[i][j] != 0)
+        continue;
+      FlipInfo = GetFlips(i, j);
+      if (isValidMove(Move, FlipInfo))
         return true;
     }
   return false;
@@ -201,10 +223,16 @@ int randrange(int lower, int upper)
 
 int main(int argc, char *argv[])
 {
+  /* Set the seed for rand() */
+  srand(time(0));
+
+  GridInfo FlipInfo = New_GridInfo();
+
   /* Initialise all grid tiles to blank. */
   for (int i = 0; i < GRID_SIZE; ++i)
     for (int j = 0; j < GRID_SIZE; ++j)
       grid[i][j] = 0;
+
   /* Initialise starting grid positions. */
   grid[GRID_SIZE / 2 - 1][GRID_SIZE / 2 - 1] = 1;
   grid[GRID_SIZE / 2][GRID_SIZE / 2] = 1;
@@ -212,18 +240,34 @@ int main(int argc, char *argv[])
   grid[GRID_SIZE / 2][GRID_SIZE / 2 - 1] = -1;
 
   int TurnCount = 0;
-  while (CanPlay())
+  bool canPlay = true;
+  do
   {
     ++TurnCount;
-    printf("Turn %d\n", TurnCount);
+    printf("Turn %d. %s to play\n", TurnCount, Player1Turn ? "W" : "B");
     PlayerColour = Player1Turn ? -1 : 1;
+    canPlay = CanPlay(FlipInfo);
+    if (!canPlay)
+      break;
     PrintGrid(grid);
-    int *Move = GetPlayerMove();
-    /* int Move[2] = {randrange(0, 7), randrange(0, 7)}; */
-    /* printf("\n"); */
-    DoPlayerMove(Move);
-  }
+    int *Move;
+    /* int Move[2]; */
+    bool validMove = true;
+    do
+    {
+      Move = GetPlayerMove();
+      /* Move[0] = randrange(0, 7); */
+      /* Move[1] = randrange(0, 7); */
+      FlipInfo = GetFlips(Move[0], Move[1]);
+      validMove = isValidMove(Move, FlipInfo);
+      if (!validMove)
+        printf("Invalid move!\n");
+    } while (!validMove);
 
+    DoPlayerMove(Move, FlipInfo);
+  } while (canPlay);
+
+  PrintGrid(grid);
   int Player1Score = GetScore(-1);
   int Player2Score = GetScore(1);
 
